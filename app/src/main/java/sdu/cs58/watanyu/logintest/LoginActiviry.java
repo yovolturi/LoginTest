3a6e35bfb1a3b293dda723b52e989eb5b6c97c07
watanyu <myloveyoza@gmail.com>
2018-02-07 10:37:51 +0700

    Initial commit

---
+++ app/src/main/java/sdu/cs58/watanyu/logintest/LoginActiviry.java
@@ -0,0 +1,13 @@
+package sdu.cs58.watanyu.logintest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActiviry extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_activiry);
+    }
+}
